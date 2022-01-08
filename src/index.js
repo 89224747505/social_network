88a68799cd939e26798f074b1969621830cb2679
89224747505 <89224747505@mail.ru>
2022-01-08 19:44:42 +0500

    02- Add

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(document.getElementById('root')
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
 );
-
-reportWebVitals();
