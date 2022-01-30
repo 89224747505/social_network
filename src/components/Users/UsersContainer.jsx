28f0ac362f690a53091d4548b523c954f6a34320
zyryanov_ivan <89224747505@mail.ru>
2022-01-30 21:27:19 +0500

    dal layer

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -3,13 +3,13 @@ import {connect} from "react-redux";
 import {follow, setUsers, unfollow, setBlockBefore, setBlockAfter, setCurrentPage, setTotalCount, setIsFetching}  from "../../redux/userReducer";
 import axios from "axios";
 import Users from "./Users";
+import {getUsers} from "../../api/api";
 
 class UsersContainer extends React.Component {
 
     componentDidMount() {
         this.props.setIsFetching(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,{withCredentials:true})
-            .then(response => {
+        getUsers(this.props.currentPage, this.props.pageSize).then(response => {
                 this.props.setTotalCount(response.data.totalCount);
                 this.props.setUsers(response.data.items);
                 this.props.setIsFetching(false);
@@ -19,8 +19,7 @@ class UsersContainer extends React.Component {
     onClickPageChanged = (el) =>{
         this.props.setIsFetching(true);
         this.props.setCurrentPage(el);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${el}&count=${this.props.pageSize}`,{withCredentials:true})
-            .then(response => {
+        getUsers(el, this.props.pageSize).then(response => {
                 this.props.setUsers(response.data.items);
                 this.props.setIsFetching(false);
             })
